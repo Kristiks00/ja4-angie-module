Kristiks00/ja4-nginx-module

Add Makefile to autromatic installation

Makefile
@@ -1,0 +1,13 @@
+default: build
+
+start:
+	./nginx_local/sbin/angie -g "daemon off;"
+
+build:
+	./configure
+
+gen-certs:
+	openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout angie_local/conf/server.key -out angie_local/conf/server.crt -subj "/C=US/ST=State/L=City/O=Organization/OU=Department/CN=localhost"
+
+convert-crt-to-pem:
+	openssl x509 -in angie_local/conf/server.crt -out angie_local/conf/server.pem -outform PEM

Kristiks00/ja4-angie-module

Makefile
@@ -1,0 +1,13 @@
+default: build
+
+start:
+	./angie_local/sbin/angie -g "daemon off;"
+
+build:
+	./configure
+
+gen-certs:
+	openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout angie_local/conf/server.key -out angie_local/conf/server.crt -subj "/C=US/ST=State/L=City/O=Organization/OU=Department/CN=localhost"
+
+convert-crt-to-pem:
+	openssl x509 -in angie_local/conf/server.crt -out angie_local/conf/server.pem -outform PEM